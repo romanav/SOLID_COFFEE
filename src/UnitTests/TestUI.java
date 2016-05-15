ea2ddd3660dee59d9da4ec1a154e803bda45214a
romanav <romanav@il.ibm.com>
2016-05-15 10:34:08 +0300

    Added unit tests

--- src/UnitTests/TestUI.java
+++ src/UnitTests/TestUI.java
@@ -1,5 +1,6 @@
 package UnitTests;
 
+import CoffeeMachine.Interfaces.IHotWaterSource.IHotWaterSource;
 import CoffeeMachine.Interfaces.ISwitchable;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,27 +9,32 @@ import static org.junit.Assert.assertEquals;
 
 public class TestUI {
 
-    ISwitchable boiler;
+    IHotWaterSource boiler;
+    ISwitchable vessel;
     SimpleUIMoc ui;
 
     @Before
     public void init() {
         boiler = new SimpleBoilerMoc();
+        vessel = new ContainmentVesselMoc(boiler);
         ui = new SimpleUIMoc();
+        ui.addComponent(boiler);
         ui.addComponent(boiler);
     }
 
     @Test
     public void testGetComponents() {
-        assertEquals(ui.getComponents().size(), 1);
+        assertEquals(ui.getComponents().size(), 2);
     }
 
     @Test
     public void startStopBoiling() {
         ui.start();
         assertEquals(boiler.isOn(), true);
+        assertEquals(boiler.isOn(), true);
 
         ui.stop();
-        assertEquals(boiler.isOn(), false);
+        assertEquals(vessel.isOn(), false);
+        assertEquals(vessel.isOn(), false);
     }
 }
